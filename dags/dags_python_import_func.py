23512023b049d28ad9c04758fa4ea92245328d55
thbins <joons0218@gmail.com>
2025-11-15 02:12:55 +0900

    datetime에 tz 오타 수정

--- dags/dags_python_import_func.py
+++ dags/dags_python_import_func.py
@@ -7,7 +7,7 @@ from common.common_func import get_sftp
 with DAG(
     dag_id="dags_python_import_func",
     schedule="30 6 * * *",
-    start_date=pendulum.datetime(2023, 3, 1, tx="Asia/Seoul"),
+    start_date=pendulum.datetime(2023, 3, 1, tz="Asia/Seoul"),
     catchup=False
 ) as dag:
     
