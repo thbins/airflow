ff02dfa03606984e4558a7021d7fe0f02881a56e
thbins <joons0218@gmail.com>
2025-11-18 17:49:49 +0900

    value2의 xcom_pull 함수 파라미터 오타 수정

--- dags/dags_python_with_xcom_eg1.py
+++ dags/dags_python_with_xcom_eg1.py
@@ -25,7 +25,7 @@ with DAG (
     def xcom_pull(**kwargs):
         ti = kwargs['ti']
         value1 = ti.xcom_pull(key="result1", task_ids=['python_xcom_push_task1', 'python_xcom_push_task2'])
-        value2 = ti.xcom_pull(key="result2". task_ids='python_xcom_push_task1')
+        value2 = ti.xcom_pull(key="result2", task_ids='python_xcom_push_task1')
         print(value1)
         print(value2)
 
