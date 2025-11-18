af94ddd0fabf84fe6aaee9b1c305c0342cdf4ace
thbins <joons0218@gmail.com>
2025-11-18 18:26:58 +0900

    bash_command 오타 수정

--- dags/dags_bash_with_macro_eg1.py
+++ dags/dags_bash_with_macro_eg1.py
@@ -16,5 +16,5 @@ with DAG (
         env={'START_DATE':'{{ (data_interval_start.in_timezone("Asia/Seoul")) | ds }}',
              'END_DATE':'{{ (data_interval_end.in_timezone("Asia/Seoul") - macros.dateutil.relativedelta.relativedelta(days=1)) | ds }}'
              },
-        bash_command='echo "START_DATE: $START_DATE && END_DATE: $END_DATE"'
+        bash_command='echo "START_DATE: $START_DATE" && "END_DATE: $END_DATE"'
     )
